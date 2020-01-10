f157fd3df55d2c710d06214f70425b042fc4cf57
Wilmer Hernandez <wagelvis@hotmail.com>
2020-01-10 15:06:01 -0400

    Refactorización y ajustes de la validación de los bloques

--- main.js
+++ main.js
@@ -1,74 +1,6 @@
-// Importamos la librería crypto-js para la generación de la clave criptográfica
-const SHA256 = require('crypto-js/sha256');
-
-class Block {
-    // Construcción del Bloque
-    constructor(index, data, previousHash = '0'){
-        this.index = index;
-        this.date = new Date();
-        this.data = data;
-        this.previousHash = previousHash;
-        this.hash = this.createHash();
-        this.nonce = 0;
-    }
-
-    // Creación del Hash
-    createHash() {
-        return SHA256(this.index + this.previousHash + this.date + this.data + this.nonce).toString();
-    }
-
-    // Generación del Minado
-    mineBlock(difficulty) {
-        while (!this.hash.startsWith(difficulty)) {
-            this.nonce++;
-            this.hash = this.createHash();
-        }
-    }
-}
-
-// Creamos la cadena de bloques
-class Blockchain {
-    constructor(genesis, difficulty = '00'){
-        this.chain = [this.createFirstBlock(genesis)];
-        this.difficulty = difficulty;
-    }
-
-    // Creando primer bloque de la cadena
-    createFirstBlock(genesis){
-        return new Block(0, genesis);
-    }
-
-    // Último bloque de la cadena
-    getLastBlock() {
-        return this.chain[this.chain.length - 1];
-    }
-
-    // Añadir bloques a la cadena
-    addBlock(data) {
-        let prevBlock = this.getLastBlock();
-        let block = new Block(prevBlock.index + 1, data, prevBlock.hash);
-
-        // Agregamos el grado de dificultad de la cadena (que es el problema que tienen que resolver los nodos de la red)
-        block.mineBlock(this.difficulty);
-        console.log('Minado! ' +block.hash+ ' con nonce ' +block.nonce);
-        this.chain.push(block);
-    }
-
-    // Validación de la Cadena
-    isValid() {
-        for(let i=1; i < this.chain.length; i++){
-            let prevBlock = this.chain[i-1];
-            let currentBlock = this.chain[i];
-
-            if(currentBlock.previousHash !== prevBlock.hash)
-                return false;
-
-            if(currentBlock.createHash() !== currentBlock.hash)
-                return false;
-        }
-        return true;
-    }
-}
+// Importamos los módulos del bloque y la cadena
+const Block = require('./block');
+const Blockchain = require('./blockchain');
 
 // Generación de los Bloques
 let willCoin = new Blockchain('Este es el primer bloque de la cadena', '0a');
@@ -76,7 +8,7 @@ let willCoin = new Blockchain('Este es el primer bloque de la cadena', '0a');
 willCoin.addBlock('Esta es la Criptomoneda de Wilmer Hernández');
 willCoin.addBlock('willCoin se cotiza a 10.000.000 CTOs');
 willCoin.addBlock('Valor cotizado en Coinmarket: 250.000 USD');
-willCoin.addBlock({Cantidad_wC: 10});
+willCoin.addBlock({Cantidad_wC: 20});
 willCoin.addBlock('Este es el Último bloque de la cadena');
 
 // Mostramos el contenido del bloque por consola
@@ -86,5 +18,6 @@ console.log(JSON.stringify(willCoin.chain, null, 4));
 console.log('La cadena de bloques es valida? -> ' +willCoin.isValid() + ': La validación de la cadena es Positiva.');
 
 // Tratamos de alterar la cadena
-willCoin.chain[5].data = 'Estoy tratando de alterar la cadena';
+willCoin.chain[1].data = 'Estoy tratando de alterar la cadena';
+willCoin.chain[1].hash = willCoin.chain[1].createHash;
 console.log('La cadena de bloques es valida? -> ' +willCoin.isValid() + ': Alerta! Hubo un intento de alterar la cadena.');
