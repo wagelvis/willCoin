74b55cb8025a0b1b2140e6dd9324b97bb6e43a82
Wilmer Hernandez <wagelvis@hotmail.com>
2020-01-10 14:40:41 -0400

    Cambios previos a Refactorización

--- main.js
+++ main.js
@@ -76,6 +76,7 @@ let willCoin = new Blockchain('Este es el primer bloque de la cadena', '0a');
 willCoin.addBlock('Esta es la Criptomoneda de Wilmer Hernández');
 willCoin.addBlock('willCoin se cotiza a 10.000.000 CTOs');
 willCoin.addBlock('Valor cotizado en Coinmarket: 250.000 USD');
+willCoin.addBlock({Cantidad_wC: 10});
 willCoin.addBlock('Este es el Último bloque de la cadena');
 
 // Mostramos el contenido del bloque por consola
@@ -85,5 +86,5 @@ console.log(JSON.stringify(willCoin.chain, null, 4));
 console.log('La cadena de bloques es valida? -> ' +willCoin.isValid() + ': La validación de la cadena es Positiva.');
 
 // Tratamos de alterar la cadena
-willCoin.chain[1].data = 'Estoy tratando de alterar la cadena';
+willCoin.chain[5].data = 'Estoy tratando de alterar la cadena';
 console.log('La cadena de bloques es valida? -> ' +willCoin.isValid() + ': Alerta! Hubo un intento de alterar la cadena.');
